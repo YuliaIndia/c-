a8297b7e517c1ddc88fe82a0210bdaeee4da3776
yulia_india <orsun88@list.ru>
2022-06-13 09:46:40 +0200

    initial commit

---
+++ sem3/Program.cs
@@ -0,0 +1,18 @@
+﻿int FindQuart (int x, int y)
+{
+    if (x>0 && y>0) return 1;
+    if (x<0 && y>0) return 2;
+    if (x>0 && y<0) return 3;
+    if (x<0 && y<0) return 4;
+    return -1;
+}
+
+
+int x, y;
+Console.Write("Введите число x: ");
+x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число y: ");
+y = Convert.ToInt32(Console.ReadLine());
+int result = FindQuart (x, y);
+if (result == -1) Console.WriteLine("One of your numbers is 0");
+else Console.WriteLine("This dot is in " + result + " Quarter");
