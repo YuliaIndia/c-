24b3cc589d68d0accd39c0d4585f6dbccebc10b8
yulia_india <orsun88@list.ru>
2022-06-20 10:38:17 +0200

    for sem5 init commit

---
+++ sem5HW/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] CreateRandomArray (int size, int min, int max)
+{
+    int[] newArray = new int[size];
+    for (int i = 0; i<size; i++)
+    {
+        newArray[i]=new Random().Next(min, max+1);
+        Console.Write(newArray[i] + " ");
+    }
+    Console.WriteLine();
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i<array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
