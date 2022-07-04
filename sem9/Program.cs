c0e4a8166e6d3d1fd1ec5f42880db39d58f7ed87
yulia_india <orsun88@list.ru>
2022-07-04 18:48:04 +0200

    after sem9 & HW sem9

---
+++ sem9/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.   */
+
+/* void ShowNums (int n)
+{
+    if(n != 1) 
+        ShowNums(n-1);
+    Console.Write(n + " ");  
+}
+ShowNums(3);    */
+
+/* Задайте значения M и N (2 - 5). Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N (2,3,4,5).   */
+/*void ShowNumsMtoN (int m, int n)
+{
+    if(n != m-1) 
+        ShowNumsMtoN(m, n-1);
+    Console.Write(n + " ");  
+}
+ShowNumsMtoN(3,10); */
+
+/* Напишите программу, которая будет принимать на вход число 
+и возвращать сумму его цифр.    */
+/* int FindSumOfDigits(int num)
+{
+    if (num/10 > 0)
+    {
+        return FindSumOfDigits (num/10) + num%10;
+    }
+    return num;
+}
+Console.Write(FindSumOfDigits(123));    */
+
+/* int FindSumOfDigits(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    return num%10 + FindSumOfDigits (num/10);
+}
+Console.Write(FindSumOfDigits(123));
+*/
+
+/* Напишите программу, которая на вход принимает два числа A и B, 
+и возводит число А в целую степень B с помощью рекурсии.    */
+
+int Step (int a, int b)
+{
+    if(b == 0) 
+        return 1;
+    return a*Step(a, b-1);  
+}
+Console.Write(Step(5,3));
+
+
+
---
+++ sem9HW/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+/* // Решение
+int SumNumbersMtoN (int m, int n)
+{
+    if (n==m)
+        return m;
+    return n + SumNumbersMtoN(m, n-1);
+}
+int M, N;
+Console.Write("Enter 1st number: ");
+M=Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2nd number: ");
+N=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum of digits between your numbers is: "+SumNumbersMtoN(M,N));
+*/
+
+/* Задача 67: Напишите программу, которая будет принимать на вход число и возвращать кол-во его цифр.
+453 -> 3
+45 -> 2 */
+
+// Решение
+int CountNumberOfDigits (int m)
+{
+    if(m==0)
+        return 1;
+    return 1 + CountNumberOfDigits(m/10);   // я пыталась тут заменить 1 на 0, но тогда вообще в итоге получаю при любом раскладе 1...
+}
+int M;
+Console.Write("Enter few-digit number: ");
+M=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number of digits in your numbers is: "+ (CountNumberOfDigits(M)-1)); // поэтому пришлось тут вычитать эту единицу. 
+                                                                                        // Почему так работает рекурсия - не понимаю...
+                                                                                        
